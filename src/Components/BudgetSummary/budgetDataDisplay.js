aaf34095006f1e33aed51d24b81960fc72000627
innocentEdosa <innocent@yourbudgit.com>
2020-11-18 18:24:36 +0100

    top section section1

--- src/Components/BudgetSummary/budgetDataDisplay.js
+++ src/Components/BudgetSummary/budgetDataDisplay.js
@@ -32,7 +32,26 @@ BHCPF
 
 
         <div class="dataDisplay__container--bottom">
+        <div class="dataDisplay__container--bottom-section1">
+        <h6 class="section1__header">
+        50%
+        </h6>
+
+        <div class="section1__content">
+        <h6 class="section1__content--header">NHIS</h6>
+        <h6 class="section1__content--leadMajor">N17.52bn</h6>
+        <h6 class="section1__content--leadMedium">
+        Basic minimum package of health care services
+        </h6>
+        <p class="section1__content--leadMinor">
+        Free Maternal and Child health Care, facilitated referrals for Cardiovascular and diabetic disease
+        </p>
+        </div>
+
+        </div>
+        <div>
         this is the bottom section
+        </div>
         </div>
         </div>
     </div>
